6cb88b07d25bed98db0f1fb8ba4a89c42b3efae0
Oliver <oliver.henry.walters@gmail.com>
2016-03-22 23:56:45 +1100

    Script fixes

--- ADJUST.bat
+++ ADJUST.bat
@@ -2,5 +2,5 @@ cd %~p0
 REM perform a sequence of commands for ALL files given as arguments to the script
 for %%x in (%*) do (
 REM perform a sequence of actions to a STEP file
-start "" freecad.exe %%x chain.FCMacro rz- mx=1.27
+freecad.exe %%x chain.FCMacro rz+ rz+ az- ax ay tmp
 )
